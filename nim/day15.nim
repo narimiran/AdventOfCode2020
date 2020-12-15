4258cad6676445d80076c7178caaaa1bb11632f0
narimiran <narimiran@disroot.org>
2020-12-15 20:58:58 +0100

    day15: make it faster by making it dumber

--- nim/day15.nim
+++ nim/day15.nim
@@ -1,13 +1,11 @@
-import tables
-
 proc solve(input: seq[int], limit: int): int =
-  var t: Table[int, int]
+  var t = newSeq[int](limit)
   for i, n in input: t[n] = i+1
   for n in input.len+1 ..< limit:
-    let next = n - t.getOrDefault(result, n)
+    let diff = n - t[result]
     t[result] = n
-    result = next
+    result = if diff == n: 0 else: diff
 
 let input = @[9,3,1,0,8,4]
 echo input.solve 2020
-echo input.solve 30000000
+echo input.solve 30_000_000
