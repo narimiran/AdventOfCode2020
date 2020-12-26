94372ce9f7c12ac249e3499b7f7850ce99e15cdb
narimiran <narimiran@disroot.org>
2020-12-26 07:31:56 +0100

    day02.py: remove unnecessary code duplication

--- python/day02.py
+++ python/day02.py
@@ -7,11 +7,10 @@ def parse_line(line):
     lo, hi, c, pw  = re.match(pattern, line).groups()
     return int(lo), int(hi), c, pw
 
-is_valid_1 = lambda lo, hi, c, pw: lo <= pw.count(c) <= hi
-is_valid_2 = lambda lo, hi, c, pw: (pw[lo-1] == c) ^ (pw[hi-1] == c)
-
+is_valid = (lambda part, lo, hi, c, pw:
+            lo <= pw.count(c) <= hi if part == 1 else
+            (pw[lo-1] == c) ^ (pw[hi-1] == c))
 
 content = [parse_line(line) for line in open("inputs/02.txt").readlines()]
 
-print(sum(is_valid_1(*line) for line in content))
-print(sum(is_valid_2(*line) for line in content))
+print([sum(is_valid(p, *line) for line in content) for p in (1, 2)])
