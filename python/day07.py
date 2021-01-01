0eb207dde443c95bb917f1ad1605f8909ae2338f
narimiran <narimiran@disroot.org>
2021-01-01 10:59:44 +0100

    day07.py: shorter, clearer, faster

--- python/day07.py
+++ python/day07.py
@@ -1,26 +1,22 @@
 from functools import reduce
 
 
-bags = dict((line[0], list(map(str.split, line[1].split(", "))))
-            for line in (line.strip().split(" bags contain ")
-                         for line in (open("inputs/07.txt")).readlines())
-            if "no other bag" not in line[1])
+bags = {k: [(int(amount), ' '.join(color))
+            for (amount, *color, _) in map(str.split, vals.split(", "))]
+        for (k, vals) in (line.strip().split(" bags contain ")
+                          for line in open("inputs/07.txt").readlines())
+        if "no other bag" not in vals}
 
-part_1 = (lambda looking_in, seen:
-          reduce(set.union,
-                 (part_1({larger for larger, smaller in bags.items()
-                                 for bag in smaller
-                                 if ' '.join(bag[1:3]) == wanted},
-                         (seen | {wanted}))
-                  for wanted in looking_in),
-                 seen)
-          - {OUR_BAG})
+contains_ours = (lambda looking_in:
+                 any(OUR_BAG in color or contains_ours(color)
+                     for (_, color) in bags[looking_in] if color in bags.keys()))
 
-part_2 = (lambda bags, curr:
-          0 if curr not in bags.keys()
-          else sum(int(bag[0]) * (1 + part_2(bags, ' '.join(bag[1:3])))
-                   for bag in bags[curr]))
+inside_of = (lambda curr:
+             0 if curr not in bags.keys()
+             else sum(amount * (1 + inside_of(color))
+                      for (amount, color) in bags[curr]))
+
 
 OUR_BAG = "shiny gold"
-print(len(part_1({OUR_BAG}, set())))
-print(part_2(bags, OUR_BAG))
+print(sum(contains_ours(bag) for bag in bags.keys()))
+print(inside_of(OUR_BAG))
