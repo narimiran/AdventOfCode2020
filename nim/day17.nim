c7407b0394ff2ae69bb61d748b67fb82d8d48b98
narimiran <narimiran@disroot.org>
2021-01-01 13:18:20 +0100

    day17.nim: use int and IntSet instead of tuple and HashSet (2x faster)

--- nim/day17.nim
+++ nim/day17.nim
@@ -1,15 +1,21 @@
-import sets
+import intsets
+
 
 type
-  Point4d = tuple[x, y, z, w: int]
-  Grid4d = HashSet[Point4d]
+  Point4d = int
+  Grid4d = IntSet
+
+proc toPoint(x, y, z, w: int): Point4d {.inline.} =
+  ## Converts what would be a `(x, y, z, w)` tuple into
+  ## a unique integer via powers of 17.
+  4913*x + 289*y + 17*z + w
 
 proc parse(input: string): Grid4d =
   var y: int
   for line in input.lines:
     for x, c in line:
       if c == '#':
-        result.incl (x, y, 0, 0)
+        result.incl toPoint(x, y, 0, 0)
     inc y
 
 template allDirections(dims: int, body: untyped) {.dirty.} =
@@ -23,15 +29,14 @@ template allDirections(dims: int, body: untyped) {.dirty.} =
 
 proc countNeighbours(grid: Grid4d, p: Point4d, dims: int): int =
   allDirections(dims):
-    result += ord((p.x+dx, p.y+dy, p.z+dz, p.w+dw) in grid)
+    result += ord((p + toPoint(dx, dy, dz, dw)) in grid)
 
 proc nextGeneration(grid: Grid4d, dims: int): Grid4d =
   var seen: Grid4d
   for p1 in grid:
     allDirections(dims):
-      let p2: Point4d = (p1.x+dx, p1.y+dy, p1.z+dz, p1.w+dw)
-      if p2 notin seen:
-        seen.incl p2
+      let p2 = p1 + toPoint(dx, dy, dz, dw)
+      if not seen.containsOrIncl p2:
         let count = grid.countNeighbours(p2, dims)
         if count == 3 or (p2 in grid and count == 4):
           result.incl p2
